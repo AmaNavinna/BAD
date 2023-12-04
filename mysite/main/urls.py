5b08c277da792d344416e4497a61edbc8a9e5589
Ama Navinna <ama.navinna@gmail.com>
2023-12-04 15:17:10 +0530

    Update urls.py.

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.index, name='home'),  # maps to the index view for the home path
     path('home/', views.index, name='home'),
     path('movies/', views.movies, name='movies'),
-    path('now_showing/<int:now_showing_id>/', views.now_showing_details, name='movie_details'),
-    path('upcoming/<int:upcoming_id>/', views.upcoming_details, name='uocoing_details'),
     path('transaction/', views.transaction_view, name='transaction'),
     path('contactus/', views.contact_us, name='contact_us'),
+    #path('movie_details/<str:movie_type>/<int:movie_id>/', views.movie_details, name='movie_details'),
+
 
 ]
