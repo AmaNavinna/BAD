da1909aa8ee8aa567934982e2cb9e2c74cdf464f
Ama Navinna <ama.navinna@gmail.com>
2023-12-04 23:21:45 +0530

    Update views and urls.py

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import NowShowingMovie, UpcomingMovie, Contact, Transaction, Reg
